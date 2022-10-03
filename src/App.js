bb7960840c06be9ac75cb6a0f34392048964b092
Aryan Neb <aryanneb007@gmail.com>
2022-10-02 20:38:35 -0400

    Test verified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now
+          Learn React This Instant
         </a>
       </header>
     </div>
